c4eb495257034f9b0ac2b9fb837d03a795ad37a9
paulneag <paul.neag@elearningsoftware.ro>
2018-04-10 11:23:07 +0300

    change version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018041001;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2018041002;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;        // Requires this Moodle version 2.9
 $plugin->component = 'local_plugin01';      // Full name of the plugin (used for diagnostics)
